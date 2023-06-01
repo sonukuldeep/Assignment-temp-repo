135367f485db4a9f5a7c57014d3d9f227840721c
sonukuldeep <sonukuldeep@outlook.com>
2023-06-01 16:19:53 +0530

    icon update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Route, Routes, Link } from "react-router-dom"
 import { useRef, useState } from 'react'
-import { BsMap } from 'react-icons/bs';
+import { BsMap, BsPinMap } from 'react-icons/bs';
 import { MdOutlineContactEmergency } from 'react-icons/md';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineGithub } from 'react-icons/ai';
 import { Toaster } from 'react-hot-toast';
 import Contacts from './components/Contacts';
 import Maps from './components/Maps';
@@ -35,7 +36,8 @@ const App = () => {
         <div className='hidden md:block bg-gray-300 max-w-fit md:max-w-[200px] flex-1 md:border-r border-blue-500'>
           <Link to='/'><div className='flex items-center text-center md:border-b border-blue-500 text-blue-500 hover:text-gray-300 hover:bg-blue-500 py-2 transition-all'><MdOutlineContactEmergency size="1.4em" className='mx-2' /><span className='hidden md:inline'>Contact</span></div></Link>
           <Link to='/charts'><div className='flex items-center text-center md:border-b border-blue-500 text-blue-500 hover:text-gray-300 hover:bg-blue-500 py-2 transition-all'><BsMap size="1.4em" className='mx-2' /><span className='hidden md:inline'>Charts</span></div></Link>
-          <Link to='/maps'><div className='flex items-center text-center md:border-b border-blue-500 text-blue-500 hover:text-gray-300 hover:bg-blue-500 py-2 transition-all'><BsMap size="1.4em" className='mx-2' /><span className='hidden md:inline'>Maps</span></div></Link>
+          <Link to='/maps'><div className='flex items-center text-center md:border-b border-blue-500 text-blue-500 hover:text-gray-300 hover:bg-blue-500 py-2 transition-all'><BsPinMap size="1.4em" className='mx-2' /><span className='hidden md:inline'>Maps</span></div></Link>
+          <a href="https://github.com/sonukuldeep/Assignment-temp-repo" target="_blank" rel="noopener noreferrer"><div className='flex items-center text-center md:border-b border-blue-500 text-blue-500 hover:text-gray-300 hover:bg-blue-500 py-2 transition-all'><AiOutlineGithub size="1.4em" className='mx-2' /><span className='hidden md:inline'>Link to my repo</span></div></a>
         </div>
 
         <div className="root md:hidden">
@@ -47,7 +49,7 @@ const App = () => {
             <div ref={dropdown} className="pages flex flex-col items-center overflow-hidden w-0 bg-gray-100 transition-all">
               <Link className='w-full border-r border-b border-blue-500' to='/'><div className='flex items-center text-center text-blue-500 hover:text-gray-300 hover:bg-blue-500 py-2 transition-all'><MdOutlineContactEmergency size="1.4em" className='mx-2' /><span className='md:inline'>Contact</span></div></Link>
               <Link className='w-full border-r border-b border-blue-500' to='/charts'><div className='flex items-center text-center text-blue-500 hover:text-gray-300 hover:bg-blue-500 py-2 transition-all'><BsMap size="1.4em" className='mx-2' /><span className='md:inline'>Charts</span></div></Link>
-              <Link className='w-full border-r border-b border-blue-500' to='/maps'><div className='flex items-center text-center text-blue-500 hover:text-gray-300 hover:bg-blue-500 py-2 transition-all'><BsMap size="1.4em" className='mx-2' /><span className='md:inline'>Maps</span></div></Link>
+              <Link className='w-full border-r border-b border-blue-500' to='/maps'><div className='flex items-center text-center text-blue-500 hover:text-gray-300 hover:bg-blue-500 py-2 transition-all'><BsPinMap size="1.4em" className='mx-2' /><span className='md:inline'>Maps</span></div></Link>
             </div>
           </div>
         </div>
@@ -59,7 +61,7 @@ const App = () => {
             <Route path="/maps" element={<Maps />} />
           </Routes>
         </div>
-                    <Toaster position="bottom-center" />
+        <Toaster position="bottom-center" />
 
       </main>
     </>
